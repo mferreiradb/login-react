034af9cda52abfcfb4761ef3bc1a1eb90156e671
Mauricio <mauhmg@gmail.com>
2022-10-28 21:13:57 -0300

    First commit

---
+++ client/READEME.md
@@ -0,0 +1,10 @@
+**TÉCNOLOGIAS UTILIZADAS**
+
+- Vite
+- Formik para fazer o formulário
+
+    npm i formik
+
+- Yup para validação
+
+    npm i yup
---
+++ client/src/App.jsx
@@ -0,0 +1,82 @@
+import './App.css'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+
+function App() {
+
+  const handleClickLogin = (values) => console.log(values)
+
+
+  const validationLogin = yup.object().shape({
+    email: yup.string().email().required('Este campo é obrigatório'),
+    password: yup.string().min(8).required('Este campo é obrigatório'),
+  });
+
+  const handleClickCadastro = (values) => console.log(values)
+
+
+  const validationCadastro = yup.object().shape({
+    email: yup.string().email().required('Este campo é obrigatório'),
+    password: yup.string().min(8).required('Este campo é obrigatório'),
+    confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'As senhas não são iguais'),
+  });
+
+  return (
+    <div className='container'>
+      <h1>Login</h1>
+
+      <Formik
+        initialValues={{}}
+        onSubmit={handleClickLogin}
+        validationSchema={validationLogin}
+      >
+        <Form className='login-form'>
+          <div className='login-form-group'>
+            <Field name='email' className='form-field' placeholder='Email' />
+
+            <ErrorMessage component='span' name='email' className='form-error' />
+          </div>
+
+          <div className='login-form-group'>
+            <Field name='password' className='form-field' placeholder='Senha' />
+
+            <ErrorMessage component='span' name='password' className='form-error' />
+          </div>
+
+          <button className='button' type='submit'>Login</button>
+        </Form>
+      </Formik>
+
+      <h1>Cadastro</h1>
+
+      <Formik
+        initialValues={{}}
+        onSubmit={handleClickCadastro}
+        validationSchema={validationCadastro}
+      >
+        <Form className='login-form'>
+          <div className='login-form-group'>
+            <Field name='email' className='form-field' placeholder='Email' />
+
+            <ErrorMessage component='span' name='email' className='form-error' />
+          </div>
+
+          <div className='login-form-group'>
+            <Field name='password' className='form-field' placeholder='Senha' />
+
+            <ErrorMessage component='span' name='password' className='form-error' />
+          </div>
+
+          <div className='login-form-group'>
+            <Field name='confirmPassword' className='form-field' placeholder='Confirme sua Senha' />
+
+            <ErrorMessage component='span' name='confirmPassword' className='form-error' />
+          </div>
+
+          <button className='button' type='submit'>Login</button>
+        </Form>
+      </Formik>
+    </div>
+  )
+}
+export default App;
